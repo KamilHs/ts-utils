305da17e29489a8fcee6f7758bc386a3612ff4d1
KamilHs <kamilsalimli1940@gmail.com>
2022-11-18 19:33:51 +0400

    add 110

---
+++ src/ts-challenge/medium/110-capitalize.ts
@@ -0,0 +1,5 @@
+type MyCapitalize<T extends string> = T extends `${infer F}${infer Rest}`
+  ? `${Uppercase<F>}${Rest}`
+  : T;
+
+type capitalized = MyCapitalize<'hello world'>; // expected to be 'Hello world'
