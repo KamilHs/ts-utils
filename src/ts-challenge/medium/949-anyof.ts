c488782f810b231a1de728b64b34d6ea1022791e
KamilHs <kamilsalimli1940@gmail.com>
2022-11-19 14:37:55 +0400

    add 949

---
+++ src/ts-challenge/medium/949-anyof.ts
@@ -0,0 +1,10 @@
+type AnyOf<T extends any[]> = T extends []
+  ? false
+  : T extends [infer Head, ...infer Rest]
+  ? Head extends 0 | '' | false | [] | Record<string, never> | undefined | null
+    ? AnyOf<Rest>
+    : true
+  : never;
+
+type Sample1 = AnyOf<[1, '', false, [], {}]>; // expected to be true.
+type Sample2 = AnyOf<[0, '', false, [], {}]>; // expected to be false.
