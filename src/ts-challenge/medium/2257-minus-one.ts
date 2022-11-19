8425deb1b6b76df4f98e62c9b2c33fdc07ed8ecd
KamilHs <kamilsalimli1940@gmail.com>
2022-11-20 01:56:35 +0400

    add 2257

---
+++ src/ts-challenge/medium/2257-minus-one.ts
@@ -0,0 +1,10 @@
+type Arr<N extends number, T extends any[] = []> = T['length'] extends N
+  ? T
+  : Arr<N, [...T, any]>;
+
+type MinusOne<N extends number> = Arr<N> extends [any, ...infer U]
+  ? U['length']
+  : never;
+  
+type Zero = MinusOne<1>; // 0
+type FiftyFour = MinusOne<55>; // 54
