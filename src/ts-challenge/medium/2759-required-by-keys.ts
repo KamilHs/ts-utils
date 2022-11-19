552d3b802681c72e155b74df72f5aaa38daecb22
KamilHs <kamilsalimli1940@gmail.com>
2022-11-20 02:05:46 +0400

    add 2759

---
+++ src/ts-challenge/medium/2759-required-by-keys.ts
@@ -0,0 +1,15 @@
+type RequiredByKeys<T, K extends keyof T = keyof T> = Omit<T, K> & {
+  [P in K]-?: T[P];
+} extends infer R
+  ? {
+      [P in keyof R]: R[P];
+    }
+  : never;
+
+interface User {
+  name?: string;
+  age?: number;
+  address?: string;
+}
+
+type UserRequiredName = RequiredByKeys<User, 'name'>; // { name: string; age?: number; address?: string }
