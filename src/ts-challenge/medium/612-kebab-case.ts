52b7361b4ea6e30a415b05fa6a62814b3242376a
KamilHs <kamilsalimli1940@gmail.com>
2022-11-19 14:31:14 +0400

    add 612

---
+++ src/ts-challenge/medium/612-kebab-case.ts
@@ -0,0 +1,20 @@
+type KebabCase<
+  S extends string,
+  P extends string = never,
+> = S extends `${infer C}${infer R}`
+  ? C extends '-'
+    ? KebabCase<R, `${P}${C}`>
+    : Uppercase<C> extends C
+    ? [P] extends [never]
+      ? KebabCase<R, Lowercase<C>>
+      : KebabCase<R, `${P}-${Lowercase<C>}`>
+    : [P] extends [never]
+    ? KebabCase<R, C>
+    : KebabCase<R, `${P}${C}`>
+  : P;
+
+type FooBarBaz = KebabCase<'FooBarBaz'>;
+const foobarbaz: FooBarBaz = 'foo-bar-baz';
+
+type DoNothing = KebabCase<'do-nothing'>;
+const doNothing: DoNothing = 'do-nothing';
