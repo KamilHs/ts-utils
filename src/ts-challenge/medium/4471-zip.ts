7bcfb8ce5aa1c9bf70df09e03f68b9b491bceb67
KamilHs <kamilsalimli1940@gmail.com>
2022-11-20 22:44:30 +0400

    add 4471

---
+++ src/ts-challenge/medium/4471-zip.ts
@@ -0,0 +1,10 @@
+type Zip<T extends any[], U extends any[]> = U extends []
+  ? []
+  : T extends [infer TF, ...infer TR]
+  ? U extends [infer UF, ...infer UR]
+    ? [[TF, UF], ...Zip<TR, UR>]
+    : []
+  : [];
+type exp = Zip<[1, 2], [true, false]>; // expected to be [[1, true], [2, false]]
+
+export {};
