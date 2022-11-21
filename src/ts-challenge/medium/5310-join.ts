c92b4c064535bea308c548e01ce05465c71060ae
KamilHs <kamilsalimli1940@gmail.com>
2022-11-22 00:19:38 +0400

    add 5310

---
+++ src/ts-challenge/medium/5310-join.ts
@@ -0,0 +1,15 @@
+type Join<T, U extends string | number> = T extends [
+  infer F extends string,
+  ...infer R,
+]
+  ? R extends []
+    ? F
+    : `${F}${U}${Join<R, U>}`
+  : never;
+
+type Res = Join<['a', 'p', 'p', 'l', 'e'], '-'>; // expected to be 'a-p-p-l-e'
+type Res1 = Join<['Hello', 'World'], ' '>; // expected to be 'Hello World'
+type Res2 = Join<['2', '2', '2'], 1>; // expected to be '21212'
+type Res3 = Join<['o'], 'u'>; // expected to be 'o'
+
+export {};
