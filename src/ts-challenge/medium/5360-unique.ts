483932e2033ddec16f2c9f38d5dc00b6f3787484
KamilHs <kamilsalimli1940@gmail.com>
2022-12-01 22:43:52 +0400

    add 5360

---
+++ src/ts-challenge/medium/5360-unique.ts
@@ -0,0 +1,18 @@
+type Includes<T, U> = U extends [infer F, ...infer R]
+  ? (<V>() => V extends F ? 0 : 1) extends <V>() => V extends T ? 0 : 1
+    ? true
+    : Includes<T, R>
+  : false;
+
+type Unique<T extends unknown[], U extends unknown[] = []> = T extends [
+  infer F,
+  ...infer R,
+]
+  ? Unique<R, Includes<F, U> extends true ? U : [...U, F]>
+  : U;
+  
+type Res = Unique<[1, 1, 2, 2, 3, 3]>; // expected to be [1, 2, 3]
+type Res1 = Unique<[1, 2, 3, 4, 4, 5, 6, 7]>; // expected to be [1, 2, 3, 4, 5, 6, 7]
+type Res2 = Unique<[1, 'a', 2, 'b', 2, 'a']>; // expected to be [1, "a", 2, "b"]
+type Res3 = Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>; // expected to be [string, number, 1, "a", 2, "b"]
+type Res4 = Unique<[unknown, unknown, any, any, never, never]>; // expected to be [unknown, any, never]
