811d8e5a8232f4421fefedf2ffbddbedc4d9140c
KamilHs <kamilsalimli1940@gmail.com>
2023-03-14 00:39:23 +0400

    add 89

---
+++ src/ts-challenge/hard/89-required-keys.ts
@@ -0,0 +1,7 @@
+type RequiredKeys<T extends object> = keyof {
+  [K in keyof T as Pick<T, K> extends Required<Pick<T, K>> ? K : never]: T[K];
+};
+
+type I = RequiredKeys<{ foo: number; bar?: string }>; // expected to be { foo: number }
+
+export {};
